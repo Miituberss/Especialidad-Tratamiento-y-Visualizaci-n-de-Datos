5fc72d381701eec5bbc0c6de92b48f198e82de17
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2025-08-05 21:58:02 +0200

    Update queries.sql

--- Sprint_3_Tratamiento_de_Datos/Lab_3_Analisis_de_bases_de_datos_de_clientes_con_PostgreSQL/Reto_proyecto_Desarrolla_una_base_de_datos_en_PostgreSQL/queries.sql
+++ Sprint_3_Tratamiento_de_Datos/Lab_3_Analisis_de_bases_de_datos_de_clientes_con_PostgreSQL/Reto_proyecto_Desarrolla_una_base_de_datos_en_PostgreSQL/queries.sql
@@ -6,18 +6,18 @@ GROUP BY client_name;
 
 -- Escribir una consulta para sumar el total de ventas por cliente y por industria.
 SELECT c.industry, c.client_name, SUM(s.sale_amount) AS total_ventas
-		FROM sales s
-		    JOIN clients c ON s.client_id = c.client_id
+	FROM sales s
+	    JOIN clients c ON s.client_id = c.client_id
 GROUP BY c.client_id, c.client_name, c.industry
 ORDER BY c.industry, c.client_id;
 
 -- Escribir una consulta para listar todas las interacciones y ventas para un cliente específico (utilizar subqueries si es necesario).
 WITH  interacciones_cte AS (
-		SELECT client_id, date AS fecha, 'Interacción' AS tipo, interaction_type AS detalle
-				FROM interactions
+	SELECT client_id, date AS fecha, 'Interacción' AS tipo, interaction_type AS detalle
+		FROM interactions
 ), ventas_cte AS (
-		SELECT client_id, sale_date AS fecha, 'Venta' AS tipo, CONCAT('Monto: $', sale_amount) AS detalle
-				FROM sales
+	SELECT client_id, sale_date AS fecha, 'Venta' AS tipo, CONCAT('Monto: $', sale_amount) AS detalle
+		FROM sales
 )
 	
 SELECT tipo, fecha, detalle 
@@ -33,18 +33,18 @@ ORDER BY fecha;
 
 -- Escribir una consulta para determinar el promedio mensual de ventas y compararlo con el mes anterior (utilizar funciones de ventana).
 WITH  promedio_mensual AS (
-		SELECT DATE_TRUNC('month', sale_date) AS mes, ROUND(AVG(sale_amount), 2) AS promedio
-				FROM sales
-		GROUP BY DATE_TRUNC('month', sale_date)
+	SELECT DATE_TRUNC('month', sale_date) AS mes, ROUND(AVG(sale_amount), 2) AS promedio
+		FROM sales
+	GROUP BY DATE_TRUNC('month', sale_date)
 )
 	
 SELECT mes, promedio, LAG(promedio) OVER (ORDER BY mes) AS promedio_mes_anterior, ROUND(promedio - LAG(promedio) OVER (ORDER BY mes), 2) AS diferencia
-		FROM promedio_mensual;
+	FROM promedio_mensual;
 
 -- Utilizar JOINs para combinar datos de clients, interactions, y sales y mostrar un resumen completo de la actividad del cliente.
 SELECT c.client_id, c.client_name, c.industry, SUM(s.sale_amount) AS ventas_totales, i.interaction_type, COUNT(i.interaction_id) AS nro_interacciones
-    FROM clients c
-        LEFT JOIN interactions i ON c.client_id = i.client_id
+	FROM clients c
+		LEFT JOIN interactions i ON c.client_id = i.client_id
         LEFT JOIN sales s ON c.client_id = s.client_id
 GROUP BY c.client_id, c.client_name, c.industry, i.interaction_type
 ORDER BY c.client_id;
