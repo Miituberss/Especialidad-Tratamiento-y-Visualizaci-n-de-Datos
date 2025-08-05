370ab0fedc72ec8d806516db2fbc52cb0e3a8f79
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2025-08-05 21:13:56 +0200

    Update schema.sql

--- Sprint_3._Tratamiento_de_Datos/Lab_3_Análisis_de_bases_de_datos_de_clientes_con_PostgreSQL/Reto_proyecto._Desarrolla_una_base_de_datos_en_PostgreSQL/schema.sql
+++ Sprint_3._Tratamiento_de_Datos/Lab_3_Análisis_de_bases_de_datos_de_clientes_con_PostgreSQL/Reto_proyecto._Desarrolla_una_base_de_datos_en_PostgreSQL/schema.sql
@@ -1,1 +1,31 @@
+-- Se eliminan las tablas clients, interactions y sales en orden, usando CASCADE para evitar problemas de integridad referencial si hay dependencias entre ellas.
+DROP TABLE IF EXISTS clients CASCADE;
+DROP TABLE IF EXISTS interactions CASCADE;
+DROP TABLE IF EXISTS sales CASCADE;
 
+/* Antes de cargar los ficheros se deben ejecutar los siguientes comandos para crear la BBDD
+ CREATE DATABASE customer_analysis; */
+
+-- Crear una tabla llamada clients con las siguientes columnas:
+CREATE TABLE clients (
+	client_id SERIAL PRIMARY KEY,
+	client_name VARCHAR(100),
+	industry VARCHAR(100),
+	contact_info VARCHAR(255)
+);
+
+-- Crear una tabla llamada interactions con las siguientes columnas:
+CREATE TABLE interactions (
+	interaction_id SERIAL PRIMARY KEY,
+	client_id INTEGER REFERENCES clients(client_id),
+	date DATE,
+	interaction_type VARCHAR(100)
+);
+
+--Crear una tabla llamada sales con las siguientes columnas:
+CREATE TABLE sales (
+	sale_id SERIAL PRIMARY KEY,
+	client_id INTEGER REFERENCES clients(client_id),
+	sale_amount DECIMAL(10, 2),
+	sale_date DATE
+);
